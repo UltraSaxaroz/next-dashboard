ec36b7a3f1251fff2d04815b8c95deed065fd050
UltraSaxaroz <fonbidon@gmail.com>
2024-06-18 13:35:39 +0400

    Initial commit of nextjs-dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter, Lusitana, Golos_Text, Montserrat} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ["400", "700"]});
+export const montserrat = Montserrat({subsets: ['latin'], weight: ["400", "700"]});
+export const golos = Montserrat({subsets: ['latin'], weight: ["400", "700"]});
